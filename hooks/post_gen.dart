7e9cc3faadfc39b5a6c8411377b4a640d3e97c97
Robby Surya Pratama <robby@skinmystery.com>
2023-01-08 04:51:01 +0700

    add use_freezed params

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -1,5 +1,23 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {
-  // TODO: add post-generation logic.
+void run(HookContext context) async {
+  final directory = Directory.current.path;
+  List<String> folders;
+  if (Platform.isWindows) {
+    folders = directory.split(r'\').toList();
+  } else {
+    folders = directory.split('/').toList();
+  }
+  final libIndex = folders.indexWhere((folder) => folder == 'lib');
+  final root = folders.sublist(0, libIndex).join('/').toString();
+  final buildRunnerProcess = await Process.start(
+    'flutter',
+    ['pub', 'run', 'build_runner', 'build'],
+    runInShell: true,
+    workingDirectory: root,
+  );
+  await stdout.addStream(buildRunnerProcess.stdout);
+  await stderr.addStream(buildRunnerProcess.stderr);
 }
--- hooks/pre_gen.dart
+++ hooks/pre_gen.dart
@@ -5,11 +5,15 @@ import 'package:mason/mason.dart';
 
 void run(HookContext context) {
   final featureName = context.vars['feature_name'] as String;
+  final useFreezed = context.vars['use_freezed'] as bool;
+
   final model = Map<String, dynamic>.from(context.vars['model']);
-  final modelGenerator = new ModelGenerator(featureName);
-  final entityGenerator = new ModelGenerator(featureName,true);
-  DartCode dartModelCode = modelGenerator.generateDartClasses(jsonEncode(model));
-  DartCode dartEntityCode = entityGenerator.generateDartClasses(jsonEncode(model));
+  final modelGenerator = new ModelGenerator(featureName, false, useFreezed);
+  final entityGenerator = new ModelGenerator(featureName, true, useFreezed);
+  DartCode dartModelCode =
+      modelGenerator.generateDartClasses(jsonEncode(model));
+  DartCode dartEntityCode =
+      entityGenerator.generateDartClasses(jsonEncode(model));
   context.vars = {
     ...context.vars,
     'model': dartModelCode.code,
