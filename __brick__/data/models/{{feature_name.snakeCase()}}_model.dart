03749ee8e12ddec74b4a379967d92165e366d3f7
Robby Surya Pratama <robby@skinmystery.com>
2023-01-08 05:20:01 +0700

    add use _freezed condition

--- __brick__/data/models/{{feature_name.snakeCase()}}_model.dart
+++ __brick__/data/models/{{feature_name.snakeCase()}}_model.dart
@@ -1,7 +1,9 @@
 import 'package:{{package_name}}/core/domain/entities/{{feature_name.snakeCase()}}_entity.dart';
-import 'package:equatable/equatable.dart';
-import 'package:json_annotation/json_annotation.dart';
+{{#!use_freezed}}import 'package:equatable/equatable.dart';{{/use_freezed}}
+{{#!use_freezed}}import 'package:json_annotation/json_annotation.dart';{{/use_freezed}}
+{{#use_freezed}}import 'package:freezed_annotation/freezed_annotation.dart';{{/use_freezed}}
 
+{{#use_freezed}}part '{{feature_name.snakeCase()}}_model.freezed.dart';{{/use_freezed}}
 part '{{feature_name.snakeCase()}}_model.g.dart';
 
 {{{model}}}
