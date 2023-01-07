0e663731279f07e59708256f0454d0085b2d2702
Robby Surya Pratama <robby@skinmystery.com>
2023-01-08 05:37:27 +0700

    add use _freezed condition

--- __brick__/domain/entities/{{feature_name.snakeCase()}}_entity.dart
+++ __brick__/domain/entities/{{feature_name.snakeCase()}}_entity.dart
@@ -1,3 +1,3 @@
-import 'package:equatable/equatable.dart';
-
+{{^use_freezed}}import 'package:equatable/equatable.dart';{{/use_freezed}}
+{{#use_freezed}}import 'package:freezed_annotation/freezed_annotation.dart';{{/use_freezed}}
 {{{entity}}}
